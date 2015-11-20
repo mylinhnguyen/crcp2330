9981ac43f14e23488934b3dfa08922b50f439987
mylinhnguyen <menguyen@smu.edu>
2015-11-20 10:30:26 -0600

    long needed push

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@2
+M=0 //zero out product register
+
+(loop)
+	@0
+	D=M
+	@done
+	D;JEQ	//check if done with calculation
+
+	@1
+	D=M 	//D= r2
+	@2
+	D=D+M 	//D= 0 + r2
+	M=D 	//stores current product calculation in r2
+
+	@0
+	D=M
+	D=D-1	//subtracts 1 from counter (which is r0)
+
+	@loop
+	JMP
+
+(done)
+	@done
+	JMP	//infinite loop
+
+
+
