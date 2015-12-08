7318ebff68780cce9fad01d0f6790a0ce78dc792
mylinhnguyen <menguyen@smu.edu>
2015-12-08 10:15:56 -0600

    v1 validating arguments

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,6 +1,25 @@
 #! /usr/bin/env ruby
 #no declared datatypes
 #no semicolons
-puts "Hi, what is your name?"
-name = gets.chomp
-puts "Hello #{name}, how are you"
+ 
+def args_valid?
+	return ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
+end
+
+def is_readable(path)
+  return File.readable?(path)
+end
+
+unless args_valid?
+  abort("Usage: ./assembler.rb Prog.asm")
+end
+
+asm_filename = ARGV[0]
+
+unless is_readable(asm_filename)
+  abort("#{asm_filename} not found or is unreadable")
+end
+
+puts "The contents of #{asm_filename}"
+
+
